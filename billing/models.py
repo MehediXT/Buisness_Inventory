5b458c1e8c197f6c4a345f9b6d16e80af1c58ff7
MehediXT <ormehediop@gmail.com>
2026-09-23 22:16:52 +0600

    add billing

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Biling(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
